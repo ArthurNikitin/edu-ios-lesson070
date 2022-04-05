3c991e40bfa641cf0e5a5930171edca57d8c3845
ArthurNikitin <Arthur.V.Nikitin@gmail.com>
2022-04-05 12:20:04 +0300

    Lesson 77 - ready

--- L070 - Xylophone/ViewController.swift
+++ L070 - Xylophone/ViewController.swift
@@ -1,48 +1,34 @@
-//
-//  ViewController.swift
-//  Xylophone
-//
-//  Created by Angela Yu on 28/06/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
-
 import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
-
-    var player: AVAudioPlayer?
     
+    var player: AVAudioPlayer!
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     @IBAction func keyPressed(_ sender: UIButton) {
+        var buttonTone: String
+        buttonTone = sender.currentTitle!
+        playSound(audioName: buttonTone)
         
-    print("RED pressed")
-    playSound()
+        sender.alpha = 0.5
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.alpha = 1.0
+            
+        }
+        
     }
     
-  
-    func playSound() {
-        guard let url = Bundle.main.url(forResource: "C", withExtension: "wav") else { return }
-
-        do {
-            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
-            try AVAudioSession.sharedInstance().setActive(true)
-
-
-            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
-
-
-            guard let player = player else { return }
-
-            player.play()
-
-        } catch let error {
-            print(error.localizedDescription)
-        }
+    func playSound(audioName: String) {
+        print(audioName)
+        let url = Bundle.main.url(forResource: audioName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
     }
-
 }
-
